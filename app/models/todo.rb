ae19569554047574fcf84c8f7cbccda0415e2ece
nkemjiks <mbonunkemjika@ymail.com>
2018-10-28 06:16:42 +0100

    First inittialization

---
+++ app/models/todo.rb
@@ -0,0 +1,8 @@
+class Todo < ApplicationRecord
+  # model association
+  has_many :items, dependent: :destroy
+  belongs_to :user
+
+  # validations
+  validates_presence_of :title, :created_by
+end
